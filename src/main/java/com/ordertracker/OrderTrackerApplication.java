773a61449c4d9ba217e9c7a69800ec84edc1e8b7
Samir Hesimov <samirhesimov257@gmail.com>
2026-08-24 18:11:47 +0400

    first commit

---
+++ src/main/java/com/ordertracker/OrderTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ordertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderTrackerApplication.class, args);
+    }
+
+}
