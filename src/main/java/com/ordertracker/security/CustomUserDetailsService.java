032e3aa5d6f318300a425c2a4fca42e2109922ae
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-09-02 23:34:52 +0400

    refactor: separate user details from user entity

---
+++ src/main/java/com/ordertracker/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.ordertracker.security;
+
+import com.ordertracker.auth.dao.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        return userRepository.findByEmail(username)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        "İstifadəçi tapılmadı"
+                ));
+    }
+}
